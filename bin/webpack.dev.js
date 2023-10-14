8031ac542b9911163825f94c090099b2015104af
Sean Bradley <seanwasere@gmail.com>
2023-10-14 19:56:03 +0100

    updates

--- bin/webpack.dev.js
+++ bin/webpack.dev.js
@@ -1,8 +1,15 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const fs = require("fs");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-module.exports = merge(common, {
+function getEntry() {
+  return fs
+    .readdirSync("./src/")
+    .filter((file) => file.match(/index\.(js|jsx|tsx)$/));
+}
+
+module.exports = {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
@@ -14,4 +21,41 @@ module.exports = merge(common, {
     port: 3000,
     allowedHosts: "all",
   },
-});
+  entry: "./src/" + getEntry(),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".js", ".jsx", ".tsx"],
+        },
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+              "@babel/preset-typescript",
+            ],
+            plugins: [require.resolve('react-refresh/babel')]
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/inline",
+      },
+    ],
+  },
+  plugins: [       
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../../../public/index.html"),
+    }),
+    new ReactRefreshWebpackPlugin()
+  ]
+};
--- bin/webpack.prod.js
+++ bin/webpack.prod.js
@@ -1,9 +1,55 @@
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const fs = require("fs");
 
-module.exports = merge(common, {
+function getEntry() {
+  return fs
+    .readdirSync("./src/")
+    .filter((file) => file.match(/index\.(js|jsx|tsx)$/));
+}
+
+module.exports = {
   mode: 'production',
   performance: {
     hints: false
-  }
-})
+  },
+  entry: "./src/" + getEntry(),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".js", ".jsx", ".tsx"],
+        },
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/inline",
+      },
+    ],
+  },
+  plugins: [       
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../../../public/index.html"),
+    }),
+  ],
+  output: {
+    filename: "[fullhash].bundle.js",
+    path: path.resolve(__dirname, "../../../build"),
+  },
+};
