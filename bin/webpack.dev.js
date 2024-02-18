494844790d2c2d38adee4a72fdc204faf6a69801
Sean Bradley <seanwasere@gmail.com>
2024-02-18 07:23:53 +0000

    added dotenv-webpack and updated most dependencies to latest

--- bin/webpack.dev.js
+++ bin/webpack.dev.js
@@ -1,69 +1,65 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const fs = require("fs");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-const InterpolateHtmlPlugin = require("@gozenc/interpolate-html-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const fs = require('fs')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const InterpolateHtmlPlugin = require('@gozenc/interpolate-html-plugin')
+const Dotenv = require('dotenv-webpack')
 
 function getEntry() {
-  return fs
-    .readdirSync("./src/")
-    .filter((file) => file.match(/index\.(js|jsx|tsx)$/));
+  return fs.readdirSync('./src/').filter((file) => file.match(/index\.(js|jsx|tsx)$/))
 }
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: 'development',
+  devtool: 'eval-source-map',
   devServer: {
     static: {
-      directory: path.join(__dirname, "../../../public"),
+      directory: path.join(__dirname, '../../../public')
     },
     hot: true,
     open: true,
     port: 3000,
-    allowedHosts: "all",
+    allowedHosts: 'all'
   },
-  entry: "./src/" + getEntry(),
+  entry: './src/' + getEntry(),
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         resolve: {
-          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
         },
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
-              "@babel/preset-typescript",
-            ],
-            plugins: [require.resolve("react-refresh/babel")],
-          },
-        },
+            presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-typescript'],
+            plugins: [require.resolve('react-refresh/babel')]
+          }
+        }
       },
       {
         test: /\.(sa|sc|c)ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf|png|jpg|jpeg|gif|svg|ico|glb|gltf|obj|mtl|fbx|stl|ply)$/i,
-        type: "asset/inline",
+        type: 'asset/inline'
       },
       {
         test: /\.json$/,
-        type: "json",
-      },
-    ],
+        type: 'json'
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "../../../public/index.html"),
+      template: path.resolve(__dirname, '../../../public/index.html')
     }),
     new ReactRefreshWebpackPlugin({ overlay: false }),
     new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
-      PUBLIC_URL: "",
+      PUBLIC_URL: ''
     }),
-  ],
-};
+    new Dotenv()
+  ]
+}
--- bin/webpack.prod.js
+++ bin/webpack.prod.js
@@ -1,63 +1,59 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const fs = require("fs");
-const InterpolateHtmlPlugin = require("@gozenc/interpolate-html-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const fs = require('fs')
+const InterpolateHtmlPlugin = require('@gozenc/interpolate-html-plugin')
+const Dotenv = require('dotenv-webpack')
 
 function getEntry() {
-  return fs
-    .readdirSync("./src/")
-    .filter((file) => file.match(/index\.(js|jsx|tsx)$/));
+  return fs.readdirSync('./src/').filter((file) => file.match(/index\.(js|jsx|tsx)$/))
 }
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   performance: {
-    hints: false,
+    hints: false
   },
-  entry: "./src/" + getEntry(),
+  entry: './src/' + getEntry(),
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         resolve: {
-          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
         },
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
-              "@babel/preset-typescript",
-            ],
-          },
-        },
+            presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-typescript']
+          }
+        }
       },
       {
         test: /\.(sa|sc|c)ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf|png|jpg|jpeg|gif|svg|ico|glb|gltf|obj|mtl|fbx|stl|ply)$/i,
-        type: "asset/inline",
+        type: 'asset/inline'
       },
       {
         test: /\.json$/,
-        type: "json",
-      },
-    ],
+        type: 'json'
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "../../../public/index.html"),
+      template: path.resolve(__dirname, '../../../public/index.html')
     }),
     new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
-      PUBLIC_URL: "",
+      PUBLIC_URL: ''
     }),
+    new Dotenv()
   ],
   output: {
-    filename: "[fullhash].bundle.js",
-    path: path.resolve(__dirname, "../../../build"),
-  },
-};
+    filename: '[fullhash].bundle.js',
+    path: path.resolve(__dirname, '../../../build')
+  }
+}
